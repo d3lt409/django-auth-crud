02444a8035bfa4813f621c7af4650f4972312245
Manuel Fernandez <mfernandez@grupoalto.com>
2022-11-03 12:16:27 -0500

    settings

--- web_service/settings.py
+++ web_service/settings.py
@@ -127,6 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGES = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 LOGIN_URL = '/signin'
 
 # Default primary key field type
